8855cc97d576234879225b74f4a9a9ee00f6cfbe
JeetsibeshSahoo <jeetsibes05@gmail.com>
2026-09-08 23:19:47 +0530

    add changes to the store

--- Frontend/src/app/store.js
+++ Frontend/src/app/store.js
@@ -1,9 +1,24 @@
-import { configureStore } from "@reduxjs/toolkit";
-import cartReducer from "../features/cart/cartSlice"
+import { configureStore } from "@reduxjs/toolkit"
 
+import cartReducer, {
+  CART_STORAGE_KEY,
+} from "../features/cart/cartSlice"
 
 export const store = configureStore({
-    reducer : {
-        cart : cartReducer,
-    }
-});
+  reducer: {
+    cart: cartReducer,
+  },
+})
+
+store.subscribe(() => {
+  try {
+    const cartItems = store.getState().cart.items
+
+    localStorage.setItem(
+      CART_STORAGE_KEY,
+      JSON.stringify(cartItems)
+    )
+  } catch (error) {
+    console.error("Failed to save cart to localStorage:", error)
+  }
+})
